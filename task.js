de4828cbaa706de789cee273eb34335537640eb7
Almog <48240399+OlympicAngel@users.noreply.github.com>
2023-07-04 12:15:31 +0300

    generic task class for displaying & handling events

---
+++ task.js
@@ -0,0 +1,43 @@
+class Task {
+    static mainContainer;
+    static autoID = 1;
+    /** @type <()=>{}> */
+    #drawFunc;
+    constructor(description) {
+        const desc = description.split("http")[0]; //set first part of the description to be everything BUT the image url
+        const imageUrl = "http" + description.split("http")[1]; //extract the url of the image
+
+        //create containing div & html structure
+        const div = document.createElement("div");
+        div.className = "task"
+        div.innerHTML = `<h2>Task #${Task.autoID}</h2>
+        <div>
+            <p>${desc}</p> <img src="${imageUrl}" alt="task${Task.autoID} illustration" />
+        </div>
+        <button>Draw Task!</button>
+        <canvas></canvas>`;
+        div.querySelector("button").addEventListener("click", this.canvasDraw.bind(this, div.querySelector("canvas")))
+
+        //if container exist - append div.
+        if (Task.mainContainer)
+            Task.mainContainer.append(div)
+
+
+        Task.autoID++; //increase id for later iteration
+    }
+
+    /**
+     * 
+     * @param {HTMLCanvasElement} canvas 
+     */
+    canvasDraw(canvas) {
+        //clean the canvas
+        canvas.getContext("2d").clearRect(0, 0, canvas.width, canvas.height);
+        this.#drawFunc?.call(canvas);
+    }
+
+    /** */
+    set drawer(func = (canvas) => { }) {
+        this.#drawFunc = func;
+    }
+}
